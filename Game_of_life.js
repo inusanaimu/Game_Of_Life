9de4ba36a94cd4f24b5ede8bcc2085489d8cba12
inusa naimu <inusanaimu@gmail.com>
2022-05-26 16:33:06 +0100

    working prototype

--- Game_of_life.js
+++ Game_of_life.js
@@ -1,6 +1,17 @@
-// collecting and declaring variables
-const rows = document.getElementById('row').value
-const column = document.getElementById('column').value
+const pp = console.log
+
+function startGame() {
+  // collecting and declaring variables
+  const rows = document.getElementById('row').value
+  const columns = document.getElementById('column').value
+  pp('Game start')
+  createGame(rows, columns)
+  repeat()
+}
+
+const repeat = () => {
+  setInterval(liveOrDie, 1500)
+}
 
 // classs creating the cells
 class Cell {
@@ -10,48 +21,52 @@ class Cell {
     this.row = row
     this.status = status
   }
+  hello() {
+    const state = this.status
+    console.log(`I'm a ${state} cell`)
+  }
+  position() {
+    const pos = this.cellnum
+    console.log(`I'm in cell number ${pos}`)
+  }
 }
 
-// (initializeGame) this is the container of all the created cells
+// (initializeGame) is an object
+// the property is array number e.g arr1 and
+// the value is an array containing the created cells e.g arr1:[cell, cell, cell]
 var initializeGame
 
 //  This function create the required Number of cell due to the input value
-function createGame(x, y = x) {
+function createGame(rows, colns = rows) {
   initializeGame = {}
+  const div = document.getElementById('root')
   let count = 1
-  for (let i = 1; i <= x; i++) {
+  for (let i = 1; i <= rows; i++) {
+    const inner_div = document.createElement('div')
     initializeGame['arr' + i] = []
-    for (let j = 1; j <= y; j++) {
+    for (let j = 1; j <= colns; j++) {
       let DOA = (Math.random() * 100).toFixed(0) % 2 == 0 ? 'alive' : 'dead'
       let c = new Cell(count, j, i, DOA)
       count++
+      let span = document.createElement('div')
+      let att = document.createAttribute('class')
+      att.value = 'root_span'
+      span.setAttributeNode(att)
+      if (c.status == 'alive') {
+        span.style.backgroundColor = 'white'
+      } else {
+        span.style.backgroundColor = 'black'
+      }
+      inner_div.appendChild(span)
       initializeGame['arr' + i].push(c)
     }
+    div.appendChild(inner_div)
   }
   return initializeGame
 }
 
-//  function liveOrDie1(){
-//         for(let x in initializeGame){
-//             for (let i = 0; i < (initializeGame[x]).length; i++){
-//                 console.log();
-//                 if (((initializeGame[x][i]).status == "alive")){
-//                     console.log("Living Cell")
-//                 } else {
-//                     console.log("Dead Cell")
-//                 }
-//             }
-//         }
-
-//     }
-// liveOrDie1();
-
-//
-function startGame(row, coln) {
-  createGame(row, coln)
-}
-
 function liveOrDie() {
+  pp('live or die running')
   // next time obj destruction is a better option here
   let arrKeys = Object.keys(initializeGame)
   let arrValues = initializeGame[arrKeys[0]]
@@ -147,97 +162,95 @@ function liveOrDie() {
             return 'This Cell Is Now ' + initializeGame[arrKeys[i]][j].status
           }
         }
-        console.log(kill())
+        kill()
         console.log(initializeGame[arrKeys[i]][j])
       } else {
-        function checkAround_Dead() {
-          let counter = 0
-          //checking the current array next column cell arr i, j+1
-          if (
-            initializeGame[arrKeys[i]][j + 1] != undefined &&
-            initializeGame[arrKeys[i]][j + 1].status == 'alive' &&
-            initializeGame[arrKeys[i]][j + 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the next array next column arr cell i+1, j+1
-          if (
-            initializeGame[arrKeys[i + 1]] != undefined &&
-            initializeGame[arrKeys[i + 1]][j + 1] != undefined &&
-            initializeGame[arrKeys[i + 1]][j + 1].status == 'alive' &&
-            initializeGame[arrKeys[i + 1]][j + 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the previous array next column cell arr i-1, j+1
-          if (
-            initializeGame[arrKeys[i - 1]] != undefined &&
-            initializeGame[arrKeys[i - 1]][j + 1] != undefined &&
-            initializeGame[arrKeys[i - 1]][j + 1].status == 'alive' &&
-            initializeGame[arrKeys[i - 1]][j + 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the previous array current column cell arr i-1, j
-          if (
-            initializeGame[arrKeys[i - 1]] != undefined &&
-            initializeGame[arrKeys[i - 1]][j] != undefined &&
-            initializeGame[arrKeys[i - 1]][j].status == 'alive' &&
-            initializeGame[arrKeys[i - 1]][j].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the next array current column cell arr i+1, j
-          if (
-            initializeGame[arrKeys[i + 1]] != undefined &&
-            initializeGame[arrKeys[i + 1]][j] != undefined &&
-            initializeGame[arrKeys[i + 1]][j].status == 'alive' &&
-            initializeGame[arrKeys[i + 1]][j].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the previous array previous column cell arr i-1, j-1
-          if (
-            initializeGame[arrKeys[i - 1]] != undefined &&
-            initializeGame[arrKeys[i - 1]][j - 1] != undefined &&
-            initializeGame[arrKeys[i - 1]][j - 1].status == 'alive' &&
-            initializeGame[arrKeys[i - 1]][j - 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the current array previous column cell arr i, j-1
-          if (
-            initializeGame[arrKeys[i]][j - 1] != undefined &&
-            initializeGame[arrKeys[i]][j - 1].status == 'alive' &&
-            initializeGame[arrKeys[i]][j - 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          //checking the next array previous column cell arr i+1, j+1
-          if (
-            initializeGame[arrKeys[i + 1]] != undefined &&
-            initializeGame[arrKeys[i + 1]][j - 1] != undefined &&
-            initializeGame[arrKeys[i + 1]][j - 1].status == 'alive' &&
-            initializeGame[arrKeys[i + 1]][j - 1].status != 'dead'
-          ) {
-            counter += 1
-          }
-          return counter
-        }
-        function Revive() {
-          let decisionValue = checkAround_Dead()
-          console.log(decisionValue)
-          if (decisionValue > 3) {
+        // function checkAround_Dead() {
+        //   let counter = 0
+        //   //checking the current array next column cell arr i, j+1
+        //   if (
+        //     initializeGame[arrKeys[i]][j + 1] != undefined &&
+        //     initializeGame[arrKeys[i]][j + 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i]][j + 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the next array next column arr cell i+1, j+1
+        //   if (
+        //     initializeGame[arrKeys[i + 1]] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j + 1] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j + 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i + 1]][j + 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the previous array next column cell arr i-1, j+1
+        //   if (
+        //     initializeGame[arrKeys[i - 1]] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j + 1] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j + 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i - 1]][j + 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the previous array current column cell arr i-1, j
+        //   if (
+        //     initializeGame[arrKeys[i - 1]] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j].status == 'alive' &&
+        //     initializeGame[arrKeys[i - 1]][j].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the next array current column cell arr i+1, j
+        //   if (
+        //     initializeGame[arrKeys[i + 1]] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j].status == 'alive' &&
+        //     initializeGame[arrKeys[i + 1]][j].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the previous array previous column cell arr i-1, j-1
+        //   if (
+        //     initializeGame[arrKeys[i - 1]] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j - 1] != undefined &&
+        //     initializeGame[arrKeys[i - 1]][j - 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i - 1]][j - 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the current array previous column cell arr i, j-1
+        //   if (
+        //     initializeGame[arrKeys[i]][j - 1] != undefined &&
+        //     initializeGame[arrKeys[i]][j - 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i]][j - 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   //checking the next array previous column cell arr i+1, j+1
+        //   if (
+        //     initializeGame[arrKeys[i + 1]] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j - 1] != undefined &&
+        //     initializeGame[arrKeys[i + 1]][j - 1].status == 'alive' &&
+        //     initializeGame[arrKeys[i + 1]][j - 1].status != 'dead'
+        //   ) {
+        //     counter += 1
+        //   }
+        //   return counter
+        // }
+        function revive() {
+          //   let decisionValue = checkAround_Dead()
+          //   console.log(decisionValue)
+          if (true) {
             initializeGame[arrKeys[i]][j].status = 'alive'
             return 'This Cell Is Now ' + initializeGame[arrKeys[i]][j].status
           }
           return "Thank's for your effort Inusa"
         }
+        revive()
         console.log(initializeGame[arrKeys[i]][j])
-        console.log(Revive())
       }
     }
   }
 }
-
-liveOrDie(6, 9)
